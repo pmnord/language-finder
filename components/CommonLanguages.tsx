a709dd416f4131540cbb28748e5fb3dd95e85ec6
Peter Nordman <peternordman@carenexis.com>
2021-07-03 14:43:49 -0400

    Fix language links in CommonLanguages

--- components/CommonLanguages.tsx
+++ components/CommonLanguages.tsx
@@ -3,164 +3,62 @@ import styles from '../styles/CommonLanguages.module.scss';
 export default function CommonLanguagesBanner() {
   return (
     <div className={styles.root}>
-      <span>
-        <a href='https://mylanguage.net.au/show/arq00&amp;sa=D&amp;source=editors&amp;ust=1622223435229000&amp;usg=AOvVaw3RiaXDSp4oj9sa6qCpW2PQ'>
-          &#1593;&#1585;&#1576;&#1609;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/bng00&amp;sa=D&amp;source=editors&amp;ust=1622223435229000&amp;usg=AOvVaw25a5m93wNbIYOObMFT7YrZ'>
-          &nbsp;&#2476;&#2494;&#2434;&#2482;&#2494;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/chn06&amp;sa=D&amp;source=editors&amp;ust=1622223435229000&amp;usg=AOvVaw1TKliPYP4IFkcAdbUxIgcz'>
-          &#31616;&#20307;&#20013;&#25991;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/eng00&amp;sa=D&amp;source=editors&amp;ust=1622223435230000&amp;usg=AOvVaw2GgJ01sjNFCU4f0o2o_NF-'>
-          English
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/pes00&amp;sa=D&amp;source=editors&amp;ust=1622223435230000&amp;usg=AOvVaw2gw6CGUtOlgVkchMrNicN_'>
-          &#1601;&#1575;&#1585;&#1587;&#1740;
-        </a>
-      </span>
-      <span>&nbsp;</span>
-      <span>&nbsp; </span>
-      <span>
-        <a href='https://mylanguage.net.au/show/frn00&amp;sa=D&amp;source=editors&amp;ust=1622223435230000&amp;usg=AOvVaw19eNp_FTtFbe4UCkuNpbx5'>
-          fran&ccedil;aise
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/ger00&amp;sa=D&amp;source=editors&amp;ust=1622223435230000&amp;usg=AOvVaw1v86LeIOuMDaLRCkLG2Fz-'>
-          Deutsch
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/hnd00&amp;sa=D&amp;source=editors&amp;ust=1622223435230000&amp;usg=AOvVaw1RCiroKenLbPdHmNAzlWrW'>
-          &#2361;&#2367;&#2344;&#2381;&#2342;&#2368;
-        </a>
-      </span>
-      <span>&nbsp;</span>
-      <span>&nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/inz00&amp;sa=D&amp;source=editors&amp;ust=1622223435230000&amp;usg=AOvVaw3Q2X3UPOO2hY_DZYcxk1Gd'>
-          Bahasa Indonesia
-        </a>
-      </span>
-      <span>&nbsp; </span>
-      <span>
-        <a href='https://mylanguage.net.au/show/itn00&amp;sa=D&amp;source=editors&amp;ust=1622223435231000&amp;usg=AOvVaw265Sgp3zOHwlXQrqYozD3E'>
-          Italiano
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/jpn00&amp;sa=D&amp;source=editors&amp;ust=1622223435231000&amp;usg=AOvVaw0fvWc2JUoqhmQ0L4FdXP-M'>
-          &#26085;&#26412;&#35486;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/kkn00&amp;sa=D&amp;source=editors&amp;ust=1622223435231000&amp;usg=AOvVaw1fw4u9EBn4D7P3ffWBPt8F'>
-          &#54620;&#44397;&#50612;
-        </a>
-      </span>
-      <span>&nbsp; </span>
-      <span>
-        <a href='https://mylanguage.net.au/show/mrt00&amp;sa=D&amp;source=editors&amp;ust=1622223435231000&amp;usg=AOvVaw3MDwD6pJA30V3rOhGbv34q'>
-          &#2350;&#2352;&#2366;&#2336;&#2368;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/pql00&amp;sa=D&amp;source=editors&amp;ust=1622223435231000&amp;usg=AOvVaw31UQNW6h_xPkm_dWhVV1jZ'>
-          Polski
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/por05&amp;sa=D&amp;source=editors&amp;ust=1622223435231000&amp;usg=AOvVaw1Ys4XSMsAKhKc4g6T-vEl7'>
-          Portugu&ecirc;s brasileiro
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/pnb00&amp;sa=D&amp;source=editors&amp;ust=1622223435232000&amp;usg=AOvVaw0LqOslxJ8Vnz87tgmfwX1c'>
-          &#1662;&#1606;&#1624;&#1580;&#1575;&#1576;&#1740;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/pnj00&amp;sa=D&amp;source=editors&amp;ust=1622223435232000&amp;usg=AOvVaw0f6cN42VMEqn5CvmFY-Uz2'>
-          &#2602;&#2672;&#2588;&#2622;&#2604;&#2624;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/rus00&amp;sa=D&amp;source=editors&amp;ust=1622223435232000&amp;usg=AOvVaw1QXnMQjW7t7VcPX6G9kAxK'>
-          P&#1091;&#1089;&#1089;&#1082;&#1080;&#1081;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/som00&amp;sa=D&amp;source=editors&amp;ust=1622223435232000&amp;usg=AOvVaw3JC4yirP2OW-HPZV8q6_YK'>
-          Af Soomaali
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/spn00&amp;sa=D&amp;source=editors&amp;ust=1622223435232000&amp;usg=AOvVaw1A4uyxIzIkoEs-inCuqN6C'>
-          Espa&ntilde;ol Latinoamericano
-        </a>
-      </span>
-      <span>&nbsp; </span>
-      <span>
-        <a href='https://mylanguage.net.au/show/swc00&amp;sa=D&amp;source=editors&amp;ust=1622223435232000&amp;usg=AOvVaw0fOzoOwjYVK7FBkUZmonLP'>
-          Swahili
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/tgl00&amp;sa=D&amp;source=editors&amp;ust=1622223435232000&amp;usg=AOvVaw3izFsbWy6IVkgHxpEU5W9h'>
-          Tagalog
-        </a>
-      </span>
-      <span>&nbsp; </span>
-      <span>&nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/tcv00&amp;sa=D&amp;source=editors&amp;ust=1622223435233000&amp;usg=AOvVaw29-6jDA80HeZX7pNJ7pw4V'>
-          &#2980;&#2990;&#3007;&#2996;&#3021;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/trk00&amp;sa=D&amp;source=editors&amp;ust=1622223435233000&amp;usg=AOvVaw2ZT7P61Z4S1K6Jz_cqOCiZ'>
-          T&uuml;rk&ccedil;e
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/urd00&amp;sa=D&amp;source=editors&amp;ust=1622223435233000&amp;usg=AOvVaw0raeJY2RTN_KKC_pkDLj8L'>
-          &#1575;&#1615;&#1585;&#1583;&#1615;&#1608;
-        </a>
-      </span>
-      <span>&nbsp; &nbsp;</span>
-      <span>
-        <a href='https://mylanguage.net.au/show/vie00&amp;sa=D&amp;source=editors&amp;ust=1622223435233000&amp;usg=AOvVaw3eEoUImu10ds8a5b3yAufs'>
-          Ti&#7871;ng Vi&#7879;t
-        </a>
-      </span>
+      <a href='https://mylanguage.net.au/show/arq00'>
+        &#1593;&#1585;&#1576;&#1609;
+      </a>
+      <a href='https://mylanguage.net.au/show/bng00'>
+        &nbsp;&#2476;&#2494;&#2434;&#2482;&#2494;
+      </a>
+      <a href='https://mylanguage.net.au/show/chn06'>
+        &#31616;&#20307;&#20013;&#25991;
+      </a>
+      <a href='https://mylanguage.net.au/show/eng00'>English</a>
+      <a href='https://mylanguage.net.au/show/pes00'>
+        &#1601;&#1575;&#1585;&#1587;&#1740;
+      </a>
+      <a href='https://mylanguage.net.au/show/frn00'>fran&ccedil;aise</a>
+      <a href='https://mylanguage.net.au/show/ger00'>Deutsch</a>
+      <a href='https://mylanguage.net.au/show/hnd00'>
+        &#2361;&#2367;&#2344;&#2381;&#2342;&#2368;
+      </a>
+      <a href='https://mylanguage.net.au/show/inz00'>Bahasa Indonesia</a>
+      <a href='https://mylanguage.net.au/show/itn00'>Italiano</a>
+      <a href='https://mylanguage.net.au/show/jpn00'>
+        &#26085;&#26412;&#35486;
+      </a>
+      <a href='https://mylanguage.net.au/show/kkn00'>
+        &#54620;&#44397;&#50612;
+      </a>
+      <a href='https://mylanguage.net.au/show/mrt00'>
+        &#2350;&#2352;&#2366;&#2336;&#2368;
+      </a>
+      <a href='https://mylanguage.net.au/show/pql00'>Polski</a>
+      <a href='https://mylanguage.net.au/show/por05'>
+        Portugu&ecirc;s brasileiro
+      </a>
+      <a href='https://mylanguage.net.au/show/pnb00'>
+        &#1662;&#1606;&#1624;&#1580;&#1575;&#1576;&#1740;
+      </a>
+      <a href='https://mylanguage.net.au/show/pnj00'>
+        &#2602;&#2672;&#2588;&#2622;&#2604;&#2624;
+      </a>
+      <a href='https://mylanguage.net.au/show/rus00'>
+        P&#1091;&#1089;&#1089;&#1082;&#1080;&#1081;
+      </a>
+      <a href='https://mylanguage.net.au/show/som00'>Af Soomaali</a>
+      <a href='https://mylanguage.net.au/show/spn00'>
+        Espa&ntilde;ol Latinoamericano
+      </a>
+      <a href='https://mylanguage.net.au/show/swc00'>Swahili</a>
+      <a href='https://mylanguage.net.au/show/tgl00'>Tagalog</a>
+      <a href='https://mylanguage.net.au/show/tcv00'>
+        &#2980;&#2990;&#3007;&#2996;&#3021;
+      </a>
+      <a href='https://mylanguage.net.au/show/trk00'>T&uuml;rk&ccedil;e</a>
+      <a href='https://mylanguage.net.au/show/urd00'>
+        &#1575;&#1615;&#1585;&#1583;&#1615;&#1608;
+      </a>
+      <a href='https://mylanguage.net.au/show/vie00'>Ti&#7871;ng Vi&#7879;t</a>
     </div>
   );
 }
