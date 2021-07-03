253f616bfb98e8c4547ff76f177b62111b1c3a55
Peter Nordman <peternordman@carenexis.com>
2021-07-03 15:15:09 -0400

    Add Head component to [country] with dynamic site title

--- pages/c/[country].tsx
+++ pages/c/[country].tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import styles from '../../styles/Country.module.scss';
+import Head from 'next/head';
 
 const CountryPage = () => {
   const router = useRouter();
@@ -8,6 +9,11 @@ const CountryPage = () => {
 
   return (
     <div className={styles.root}>
+      <Head>
+        <title>{country} - Watch the film JESUS in your language</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
       <h1>{country}</h1>
       <div className={styles.languagesContainer}>
         <h2>Select your language below</h2>
