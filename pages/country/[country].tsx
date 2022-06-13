e5033644b82a415e5ed3c6f66b6cdb5b76852789
Peter Nordman <peter.m.nordman@gmail.com>
2022-06-12 21:59:34 -0700

    Refactor more stuff to tailwind, improve open graph tags and semantic HTML

--- pages/country/[country].tsx
+++ pages/country/[country].tsx
@@ -14,86 +14,104 @@ interface Language {
 interface Country {
   id: string;
   name: string;
+  urlName: string;
   fullName: string;
   flagSvg: string;
   languages: Array<Language>;
   continents: Array<string>;
 }
 
-const CountryPage = ({ countryName }): JSX.Element => {
+const CountryPage: React.FunctionComponent<{ countryName: string }> = ({
+  countryName,
+}) => {
   countryName = decodeURI(countryName);
 
   const country: Country = countries[countryName];
 
   if (country === undefined) return null;
 
+  const title = `${country.name} - Watch the film JESUS in your language`;
+
+  const url = `https://freejesusfilm.netlify.app/country/${country.urlName}`;
+
   return (
-    <div className="container mx-auto px-1 text-center">
+    <>
       <Head>
-        <title>{country.name} - Watch the film JESUS in your language</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{title}</title>
+        <link rel="canonical" href={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={url} />
       </Head>
 
-      <nav className="my-2 mx-auto max-w-5xl">
-        <Link href="/">
-          <a>
-            <Image
-              className="rounded-md"
-              width={1440 * 0.4}
-              height={500 * 0.4}
-              src="/site-image.jpg"
-              alt="Learn More About Jesus Christ"
+      <div className="container mx-auto px-1 text-center">
+        <header>
+          <nav className="my-2 mx-auto max-w-5xl">
+            <Link href="/">
+              <a>
+                <Image
+                  className="rounded-md"
+                  width={1440 * 0.4}
+                  height={500 * 0.4}
+                  src="/site-image.jpg"
+                  alt="Learn More About Jesus Christ"
+                />
+              </a>
+            </Link>
+          </nav>
+        </header>
+
+        <main>
+          {country.flagSvg !== "" && (
+            <img
+              className="mx-auto mt-6 mb-3 h-40 shadow-md shadow-gray-500"
+              src={`/flags/${country.flagSvg}`}
+              alt={country.fullName}
+              title={country.fullName}
             />
-          </a>
-        </Link>
-      </nav>
+          )}
 
-      {country.flagSvg !== "" && (
-        <img
-          className="mx-auto mt-6 mb-3 h-40 shadow-md shadow-gray-500"
-          src={`/flags/${country.flagSvg}`}
-          alt={country.fullName}
-          title={country.fullName}
-        />
-      )}
+          {["Refugee", "J-Immigrants"].includes(country.name) === false ? (
+            <>
+              <h1 className="text-4xl">{country.name}</h1>
+              <h2 className="text-md mt-4">Select your language</h2>
+            </>
+          ) : (
+            <h1 className="text-3xl">Select your language</h1>
+          )}
 
-      {["Refugee", "J-Immigrants"].includes(country.name) === false ? (
-        <>
-          <h1 className="text-4xl">{country.name}</h1>
-          <h2 className="text-md mt-4">Select your language</h2>
-        </>
-      ) : (
-        <h1 className="text-3xl">Select your language</h1>
-      )}
-
-      <ul className="grid sm:grid-cols-2">
-        {country.languages &&
-          country.languages.map((language) => (
-            <li key={language.language} className="mx-auto my-5 w-fit text-lg">
-              <Link href={language.hyperlink}>
-                <a className="text-2xl">
-                  <p>{language.language}</p>
-                  {language.script.length > 0 &&
-                    language.script.toLowerCase() !== "english" && (
-                      <p>{language.script}</p>
-                    )}
-                </a>
-              </Link>
+          <ul className="mt-6 grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3">
+            {country.languages &&
+              country.languages.map((language) => (
+                <li
+                  key={language.language}
+                  className="mx-auto my-5 w-fit text-lg"
+                >
+                  <Link href={language.hyperlink}>
+                    <a className="text-2xl">
+                      <p>{language.language}</p>
+                      {language.script.length > 0 &&
+                        language.script.toLowerCase() !== "english" && (
+                          <p>{language.script}</p>
+                        )}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            <li aria-hidden>
+              <br />
             </li>
-          ))}
-        <li aria-hidden>
-          <br />
-        </li>
-      </ul>
+          </ul>
+        </main>
 
-      <Divider />
+        <Divider />
 
-      <div className="my-10">
-        <Link href="/">
-          <a>↩&nbsp;&nbsp;Back to Country List</a>
-        </Link>
+        <footer className="my-10">
+          <Link href="/">
+            <a className="no-underline">↩&nbsp;&nbsp;Back to Country List</a>
+          </Link>
+        </footer>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/language/[slug].tsx
+++ pages/language/[slug].tsx
@@ -2,8 +2,8 @@ import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
+import Divider from "../../components/Divider";
 import languagePagesData from "../../data/language-pages.json";
-import styles from "../../styles/LanguagePage.module.scss";
 
 interface LanguagePageData {
   language: string;
@@ -51,9 +51,7 @@ const LanguagePage: React.FunctionComponent<Props> = ({ data }) => {
   const url = `https://freejesusfilm.netlify.app/language/${slug}`;
 
   return (
-    <main
-      className={`${styles.root} my-0 mx-auto mt-12 flex max-w-5xl flex-col items-center px-0 py-2 text-center text-xl`}
-    >
+    <>
       <Head>
         <title>{title}</title>
         <link rel="canonical" href={url} />
@@ -61,72 +59,83 @@ const LanguagePage: React.FunctionComponent<Props> = ({ data }) => {
         <meta property="og:url" content={url} />
       </Head>
 
-      <h1>{language}</h1>
+      <main
+        className="my-0 mx-auto mt-12 flex max-w-5xl flex-col items-center px-0 py-2 text-center text-xl"
+        style={{ lineBreak: "anywhere" }}
+      >
+        <h1 className="mb-1 text-4xl">{language}</h1>
 
-      {script && <h2 className={styles.scriptTitle}>{script}</h2>}
+        {script && <h2 className="text-2xl">{script}</h2>}
 
-      <section>
-        <figure className="m-0">
-          <div
-            className={styles.video}
-            dangerouslySetInnerHTML={{
-              __html: data.videoEmbedCode,
-            }}
-          />
-          <svg viewBox="0 0 64 64" width="64" height="64">
-            <rect
-              width="64"
-              height="64"
-              rx={"6px"}
-              ry={"6px"}
-              fill="#eee"
-              // style={bgStyle}
+        <section className="my-4 w-full">
+          <figure className="mx-auto w-fit">
+            <div
+              className="ml-2 w-[calc(100vw_-_16px)] max-w-2xl"
+              dangerouslySetInnerHTML={{
+                __html: data.videoEmbedCode,
+              }}
             />
-            <path
-              d="M34.1,47V33.3h4.6l0.7-5.3h-5.3v-3.4c0-1.5,0.4-2.6,2.6-2.6l2.8,0v-4.8c-0.5-0.1-2.2-0.2-4.1-0.2 c-4.1,0-6.9,2.5-6.9,7V28H24v5.3h4.6V47H34.1z"
-              fill="#2374e1"
-            />
-          </svg>
-          <figcaption>{data.videoCaption}</figcaption>
-        </figure>
-      </section>
 
-      <section>
-        <ul style={{ listStyle: "none", textAlign: "center", padding: 0 }}>
-          <li style={listItemStyle}>
-            <label htmlFor="bible-link">{bibleText}</label>
-            <br />
-            <Link href={bibleLink}>
-              <a id="bible-link">{bibleLink}</a>
-            </Link>
-          </li>
-          <li style={listItemStyle}>
-            <label htmlFor="full-film-link">
-              {fullFilmText}
-              <br />
-            </label>
-            <Link href={fullFilmLink}>
-              <a id="full-film-link">{fullFilmLink}</a>
-            </Link>
-          </li>
-          <li style={listItemStyle}>
-            <label htmlFor="questions-link">
-              Questions?
-              <br />
-            </label>
-            <Link href={questionsLink}>
-              <a id="questions-link">{questionsLink}</a>
-            </Link>
-          </li>
-        </ul>
-      </section>
+            {/* <svg viewBox="0 0 64 64" width="64" height="64">
+              <rect
+                width="64"
+                height="64"
+                rx={"6px"}
+                ry={"6px"}
+                fill="#eee"
+                // style={bgStyle}
+              />
+              <path
+                d="M34.1,47V33.3h4.6l0.7-5.3h-5.3v-3.4c0-1.5,0.4-2.6,2.6-2.6l2.8,0v-4.8c-0.5-0.1-2.2-0.2-4.1-0.2 c-4.1,0-6.9,2.5-6.9,7V28H24v5.3h4.6V47H34.1z"
+                fill="#2374e1"
+              />
+            </svg> */}
 
-      <footer className={styles.backLinkContainer}>
+            <figcaption className="-mt-5 px-4 text-sm text-gray-600">
+              {data.videoCaption}
+            </figcaption>
+          </figure>
+        </section>
+
+        <section className="px-2 text-xl">
+          <ul style={{ listStyle: "none", textAlign: "center", padding: 0 }}>
+            <li style={listItemStyle}>
+              <label htmlFor="bible-link">{bibleText}</label>
+              <br />
+              <Link href={bibleLink}>
+                <a id="bible-link">{bibleLink}</a>
+              </Link>
+            </li>
+            <li style={listItemStyle}>
+              <label htmlFor="full-film-link">
+                {fullFilmText}
+                <br />
+              </label>
+              <Link href={fullFilmLink}>
+                <a id="full-film-link">{fullFilmLink}</a>
+              </Link>
+            </li>
+            <li style={listItemStyle}>
+              <label htmlFor="questions-link">
+                Questions?
+                <br />
+              </label>
+              <Link href={questionsLink}>
+                <a id="questions-link">{questionsLink}</a>
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </main>
+
+      <Divider />
+
+      <footer className="mb-8 text-center">
         <Link href="/">
-          <a>↩&nbsp;&nbsp;Back to Country List</a>
+          <a className="no-underline">↩&nbsp;&nbsp;Back to Country List</a>
         </Link>
       </footer>
-    </main>
+    </>
   );
 };
 
